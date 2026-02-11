7e66703db911b45ee9b5f4e4a0a0435f08f4f0f9
AhmedRabea <aarr882004@gmail.com>
2026-02-11 05:20:48 +0200

    fix merge problem

---
+++ lib/features/auth/presention/widgets/custom_social_button.dart
@@ -0,0 +1,57 @@
+
+import 'package:flutter/cupertino.dart';
+
+import '../../../../core/theme/app_colors.dart';
+import '../../../../core/theme/app_text_style.dart';
+
+class CustomSocialButton extends StatelessWidget {
+  final String text;
+  final String assetIcon;
+  final VoidCallback onPressed;
+
+  const CustomSocialButton({
+    super.key,
+    required this.text,
+    required this.assetIcon,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: double.infinity,
+        height: 56,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: AppColors.grayLight),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Image.asset(
+                  assetIcon,
+                  height: 22,
+                  width: 22,
+                ),
+              ),
+            ),
+
+            Text(
+              text,
+              style: AppTextStyles.body.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/shared/widgets/custom_button.dart
+++ lib/shared/widgets/custom_button.dart
@@ -1,41 +1,46 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:remotely_store/core/theme/app_text_style.dart';
-
 import '../../core/theme/app_colors.dart';
 
 class PrimaryButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
   final bool isLoading;
+  final Color? backgroundColor;
+  final Color? textColor;
 
   const PrimaryButton({
     super.key,
     required this.text,
     required this.onPressed,
     this.isLoading = false,
+    this.backgroundColor,
+    this.textColor,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 50,
-      child: ElevatedButton(
-        onPressed: isLoading ? null : onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: AppColors.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
+    return GestureDetector(
+      onTap: isLoading ? null : onPressed,
+      child: Container(
+        width: double.infinity,
+        height: 55,
+        decoration: BoxDecoration(
+          color: backgroundColor ?? AppColors.primary,
+          borderRadius: BorderRadius.circular(12),
         ),
+        alignment: Alignment.center,
         child: isLoading
             ? const CupertinoActivityIndicator(
           color: AppColors.black,
         )
             : Text(
           text,
-          style:AppTextStyles.body
+          style: AppTextStyles.body.copyWith(
+            color: textColor ?? AppColors.black,
+            fontWeight: FontWeight.w600,
+          ),
         ),
       ),
     );
